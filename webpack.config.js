a112d3e88abb5ee166d882749f6eea1e669f43d5
Luis Montero <lmontero@omnicomhealthgroup.com>
2018-09-02 00:15:36 -0600

    Setup to loaders and plugins handle CSS  files

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,25 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-  entry: './index.js',
+  entry: path.resolve(__dirname, 'index.js'),
   output : {
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
-  }
+  },
+  module: {
+    rules: [
+      //here add the loaders
+      {
+
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use:'css-loader'
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("css/styles.css")
+  ]
 }
